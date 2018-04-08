58e9eab2b77afe212191022e9b9a69283387024d
vipwhr <978601096@qq.com>
2018-04-09 00:15:01 +0800

    增è加消息¯¹队å的使用

--- src/main/java/com/adrian/demo/controller/BusinssController.java
+++ src/main/java/com/adrian/demo/controller/BusinssController.java
@@ -1,12 +1,24 @@
 package com.adrian.demo.controller;
 
 import com.adrian.demo.utils.JsonResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.jms.*;
+
 @RestController
 public class BusinssController {
+
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
+    @Autowired
+    private Queue queue;
 
     @RequestMapping("/buyTicket")
     JsonResult buyTicket(@RequestParam("id") Long id){
@@ -15,5 +27,15 @@ public class BusinssController {
 
     }
 
-
+    @RequestMapping("/send")
+    public void send(){
+        jmsTemplate.send("sample.queue", new MessageCreator() {
+            @Override
+            public Message createMessage(Session session) throws JMSException {
+                TextMessage textMessage = session.createTextMessage();
+                textMessage.setText("send data");
+                return textMessage;
+            }
+        });
+    }
 }
